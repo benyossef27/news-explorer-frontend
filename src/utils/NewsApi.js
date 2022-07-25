005499c3bffd9e2f909fab7ad56ec50c661b57b8
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-07-25 10:51:37 +0300

    Create NewsApi.js

---
+++ src/utils/NewsApi.js
@@ -0,0 +1,28 @@
+import { apiKey, apiUrlTweaked } from "./consts";
+
+const getArticlesFromApi = async (q) => {
+  let date = new Date();
+  const [year, month, day] = [
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+  ];
+  const response = await fetch(
+    `${apiUrlTweaked}/v2/everything?q=${q}&from=${year}-${
+      month + 1
+    }-${day}&to=${year}-${month + 1}-${
+      day - 7
+    }&pageSize=100&language=en&apiKey=${apiKey}`,
+    {
+      method: "GET",
+    }
+  );
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    return Promise.reject(response);
+  }
+};
+
+export { getArticlesFromApi };
