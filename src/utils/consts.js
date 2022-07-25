e1407a2b7fdaa9b08c9576f9d4acc5bbf3bb8592
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-07-25 10:51:23 +0300

    Create consts.js

---
+++ src/utils/consts.js
@@ -0,0 +1,6 @@
+const baseUrl = "https://api.benyo-news.students.nomoredomainssbs.ru";
+const apiKey = "3bf0fd8b88094396a542b1117b00b5f7";
+const apiUrl = "https://newsapi.org/";
+const apiUrlTweaked = "https://nomoreparties.co/news";
+
+export { baseUrl, apiKey, apiUrl, apiUrlTweaked };
