c7b04ddf2a961ee51cdfb16dce7a75ba5add3bc0
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-07-25 11:09:51 +0300

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,16 +2,33 @@ import About from "../About/About";
 import NewsCard from "../NewsCard/NewsCard";
 import NewsCardList from "../NewsCardList/NewsCardList";
 
-export default function Main({ isLoggedIn }) {
+export default function Main(props) {
+  const {
+    isLoggedIn,
+    location,
+    handleSaveArticle,
+    handleDeleteArticle,
+    searchedArticles,
+    savedArticles,
+  } = props;
   return (
     <main className="main">
-      <NewsCardList isSearching={true} isFailed={false} />
-      <NewsCardList isSearching={true} isFailed={true} />
-      <NewsCardList isSearching={false} isFailed={false}>
-        <NewsCard isLoggedIn={isLoggedIn} />
-        <NewsCard isLoggedIn={isLoggedIn} />
-        <NewsCard isLoggedIn={isLoggedIn} />
-      </NewsCardList>
+      <NewsCardList
+        isLoggedIn={false}
+        location={location}
+        searchedArticles={searchedArticles}
+        handleSaveArticle={handleSaveArticle}
+        handleDeleteArticle={handleDeleteArticle}
+        savedArticles={savedArticles}
+      />
+      <NewsCardList
+        isLoggedIn={isLoggedIn}
+        location={location}
+        searchedArticles={searchedArticles}
+        handleSaveArticle={handleSaveArticle}
+        handleDeleteArticle={handleDeleteArticle}
+        savedArticles={savedArticles}
+      />
       <About />
     </main>
   );
